50be34d8fafdb580503bb02f24f9b145dfd490ec
jsterchele <jsterchele17@cornellcollege.edu>
2015-11-03 10:48:47 -0600

    Passage retrieval and local storage

--- assets/js/app.js
+++ assets/js/app.js
@@ -74,12 +74,14 @@ function sidebarClick(id) {
   }
 }
 
-/* Larger screens get expanded layer control and visible sidebar */
+
+/* Larger screens get expanded layer control and visible sidebar 
 if (document.body.clientWidth <= 767) {
   var isCollapsed = true;
 } else {
   var isCollapsed = false;
 }
+*/
 
 /* Highlight search box text on click */
 $("#searchbox").click(function () {
@@ -155,7 +157,6 @@ $("#map-sidebar-hide-btn").on("click", function(e){
 
 $("#map-max").on("click", function(e){
   width = $("#map-container").width() / $(document).width() * 100; 
-  console.log(width);
   if(width < 90)
   {
       $("#map-container").width("100%");
@@ -171,7 +172,6 @@ $("#map-max").on("click", function(e){
 
 $("#reading-max").on("click", function(e){
   width = $("#reading-container").width() / $(document).width() * 100; 
-  console.log(width);
   if(width < 90)
   {
       $("#reading-container").width("100%");
@@ -211,6 +211,19 @@ $("#question-btn").on("click", function(e){
   $("#text-content").html(content);
 });
 
+//If the "next" button is clicked, it will load the next passage.
+$("#next").on("click", function(e){
+  passageId++;
+  getPassage(passageId);
+});
+
+//If the "Previous" button is clicked, it will load the previous passage.
+$("#previous").on("click", function(e){
+  passageId--;
+  getPassage(passageId);
+});
+
+
 /*
 This function should be loaded everytime a new passage is loaded. It will look at each content section.
 If a content section is null/empty The button should be disabled and should look grayed out
@@ -220,49 +233,168 @@ function disableButtons(){
   {
     $('#pre-reading-btn').prop('disabled', true); // Disables visually and functionally
   }
+  else
+  {
+    $('#pre-reading-btn').prop('disabled', false); // Disables visually and functionally
+  }
+  
   if($("#passage-content").text() == "" )
   {
     $('#passage-btn').prop('disabled', true); // Disables visually and functionally
   }
+  else
+  {
+    $('#passage-btn').prop('disabled', false); // Disables visually and functionally
+  }
+  
   if($("#post-content").text() == "" )
   {
     $('#post-reading-btn').prop('disabled', true); // Disables visually and functionally
   }
+  else
+  {
+    $('#post-reading-btn').prop('disabled', false); // Disables visually and functionally
+  }
+  
   if($("#media-content").text() == "" )
   {
     $('#media-btn').prop('disabled', true); // Disables visually and functionally
   }
+  else
+  {
+  $('#media-btn').prop('disabled', false); // Disables visually and functionally
+  }
+  
   if($("#question-content").text() == "" )
   {
     $('#question-btn').prop('disabled', true); // Disables visually and functionally
   }
+  else
+  {
+    $('#question-btn').prop('disabled', false); // Disables visually and functionally
+  }
+  
+  if(passageId == numPassages)
+  {
+    $('#next').prop('disabled', true); //Disables the next button if the user is at the end of the book.
+  }
+  else
+  {
+    $('#next').prop('disabled', false);
+  }
+  
+  if(passageId == 0)
+  {
+    $('#previous').prop('disabled', true); //Disables the previous button if the user is at the beginning of the book.
+  } 
+  else
+  {
+    $('#previous').prop('disabled', false);
+  }
 }
 
 
-        /**This function grabs a passage by its Passage Number.
-        It will then set all of the HTML reading container fields.
-        @param pasNum the number of the passage.*/
-        function getPassage(pasNum){
-        //This grabs a Service that given a passage number, will return all of the infomration regarding the passage.
-        $.getJSON('http://imaginingancientcorinth.com/cms/rest/views/rest_api?filters[num]='+pasNum , function (data) {
-            $("#passageNumber").html(data[0].passageNumber);
-            $("#text-title-name").html(data[0].node_title);
-            $("#pre-content").html(data[0].pre_q);
-            $("#post-content").html(data[0].post_q);
-            $("#passage-content").html(data[0].body);
-            //$("#question-content").html(data[0].question);
-            //$("#media-content").html();
-            var content = $('#passage-content').html();
-            $("#text-content").html(content);
-            //if the site has gps data, pan to the site.
-            if(data[0].lat != null && data[0].lon != null)
-             { 
-                map.panTo([data[0].lon, data[0].lat], 
-                {
-                    animate:true, 
-                    duration:3
-                });
-             }
-                disableButtons();
-          });
-        }
+/**This function grabs a passage by its Passage Number.
+It will then set all of the HTML reading container fields.
+@param pasId the number of the passage.*/
+function getPassage(passageId){
+
+//saves the passageIdNum into local storage if possible.
+savePassageID(passageId);
+
+//Changes the passage ID into a passage Number
+pasNum = passageNumbers[passageId].passage_number;
+
+//This grabs a Service that given a passage number, will return all of the infomration regarding the passage.
+$.getJSON('http://imaginingancientcorinth.com/cms/rest/views/rest_api?filters[num]='+pasNum , function (data) {
+    $("#passageNumber").html(data[0].passageNumber);
+    $("#text-title-name").html(data[0].node_title);
+    $("#pre-content").html(data[0].pre_q);
+    $("#post-content").html(data[0].post_q);
+    $("#passage-content").html(data[0].body);
+    //$("#question-content").html(data[0].question);
+    $("#media-content").html(data[0].media);
+    var content = $('#passage-content').html();
+    $("#text-content").html(content);
+    //if the site has gps data, pan to the site.
+    if(data[0].lat != null && data[0].lon != null)
+     { 
+        map.panTo([data[0].lon, data[0].lat], 
+        {
+            animate:true, 
+            duration:3
+        });
+     }  //checks if any of the buttons must be disabled.
+        disableButtons();
+  });
+}
+
+/**This function grabs a passage by its Passage Number.
+It will then set all of the HTML reading container fields.
+@param pasId the number of the passage.*/
+function getPassage1(pasNum){
+//This grabs a Service that given a passage number, will return all of the infomration regarding the passage.
+$.getJSON('http://imaginingancientcorinth.com/cms/rest/views/rest_api?filters[num]='+pasNum , function (data) {
+    $("#passageNumber").html(data[0].passageNumber);
+    $("#text-title-name").html(data[0].node_title);
+    $("#pre-content").html(data[0].pre_q);
+    $("#post-content").html(data[0].post_q);
+    $("#passage-content").html(data[0].body);
+    //$("#question-content").html(data[0].question);
+    $("#media-content").html(data[0].media);
+    var content = $('#passage-content').html();
+    $("#text-content").html(content);
+    //if the site has gps data, pan to the site.
+    if(data[0].lat != null && data[0].lon != null)
+     { 
+        map.panTo([data[0].lon, data[0].lat], 
+        {
+            animate:true, 
+            duration:3
+        });
+     }
+        disableButtons();
+  });
+  passageId = 0;
+  savePassageID(passageId);
+}
+
+
+/***
+This function will save the passageID via HTML 5's internal storage feature 
+ ***/
+function savePassageID(id){ 
+ // Check browser support
+if (typeof(Storage) !== "undefined") {
+    // Store
+    localStorage.setItem("passageIdNum", id);
+  } 
+}
+
+
+/***
+This function will retrieve the PassageID via HTML 5's internal storage feature
+ ***/
+ function getPassageID(){
+  // Check browser support
+if (typeof(Storage) !== "undefined") {
+  //return the passageIdNum
+    return localStorage.getItem("passageIdNum");
+ }
+ else
+ {
+    return 0;
+ }
+}
+
+/**Retrieves the passage **/
+function retrievePassage(){
+    if(getPassageID() == 'null' )
+    {
+      getPassage1('2.1.1');
+    }
+    else
+    {
+      getPassage(getPassageID());
+    }
+}
